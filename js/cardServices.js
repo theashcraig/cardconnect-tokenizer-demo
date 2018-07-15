db1775369f7dedd3bc72efdbb899be2d0fdbc7ed
Ash Craig <ashcraig@Ashs-MacBook-Pro.local>
2018-07-15 10:32:28 -0700

    update

--- js/cardServices.js
+++ js/cardServices.js
@@ -2,7 +2,10 @@
     Quick & Dirty Production Demo - CardConnect's Hosted iFrame Tokenizer
     Ash Craig, FirmPOS Software
 
-    Use as you like. Hope it helps.
+    Demonstrates how a card number gets tokenized so you can perform sales or auths
+    from manual keyed card numbers.
+
+    No warranties. Just sample code. Use at your risk. Use as you like. Hope it helps.
 
     @uses:  Bootstrap
     jQuery
@@ -16,10 +19,13 @@ var cardServices = {
     init : function(){
         var self = this;
 
+        //show the initial loading msg
         self.working(true, 'Loading...');
 
         //initially hide the error handling rows
         $(".cd-err-row").hide();
+
+        //hide the token display element
         $("#myToken").hide();
 
         //add the listener for messages from the frame
@@ -43,11 +49,14 @@ var cardServices = {
                 //show the error msg
                 $("#cc_error").show();
 
-                //remove the msg after 3 seconds
+                //remove the msg after about 3 seconds
                 setTimeout(function(){
                     $("#cc_error").fadeOut();
                 },3400);
 
+                //there's an issue... let the user know
+                //we also use this as a cover while the hosted iFrame
+                //loads and gets styled in the background
                 self.working(true, 'Issue...');
 
                 //reload the iframe
@@ -61,10 +70,15 @@ var cardServices = {
 
         }, false);
 
+        //this creates the frame
         self.setFrame();
 
+        //sets the global handling for
+        //proper token validation
         TOKEN_PROPERTY = self.scrubInput(self.getQueryVariable("tokenpropname", "message"), 30, "message", /^[0-9a-zA-Z]+$/);
 
+        //simple example of ajax call to pass token
+        //exp date and cvvc to server
         $("#btn_submit").off("click").on("click", function(){
             self.submitButton();
         });
@@ -121,19 +135,26 @@ var cardServices = {
     setFrame : function(){
         var self = this;
 
+        //style the frame to match your form
         var style = '&css=input:focus{outline: none;}body{margin:0!important;}';
         style = style + 'input{margin: 0px; padding: 12px 10px; font-size: 15px;';
         style = style + 'font-family: arial; background-color: whitesmoke; border: none; width: 1000px;}';
 
+        //create the querystr parameters to pass to the frame
         var opts = '?invalidinputevent=true';
         opts = opts + '&enhancedresponse=true';
         opts = opts + '&formatinput=true';
         opts = opts + style;
 
+        //the complete url for the frame
         var url = 'https://fts.cardconnect.com:6443/itoke/ajax-tokenizer.html' + opts;
 
+        //load the frame
         $("#tokenframe").attr("src", url);
 
+        //keep the working element visible for another 300ms
+        //as the frame finishes loading styles
+        //you can adjust this time if it's not enough
         setTimeout(function(){
             self.working(false);
         },300);
@@ -174,13 +195,16 @@ var cardServices = {
             //if successful, the returned status will equal "success"
             if (data.status == "success") {
 
+                //complete the task and display the completion msg
                 $("#card_container").html($("#template_card_completed").html());
+
+                //remove unnecessary items
                 $("#myToken").remove();
                 $("#btn_submit").remove();
 
             } else {
 
-                //on error, you will receieve the msg
+                //on error, you should display a mag from the server
                 alert(data.msg);
             }
         });
